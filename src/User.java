6f0375bf75fd885363ac6c81dd27b5cce177fc63
lei0417 <2552625354@qq.com>
2020-12-28 22:43:20 +0800

    1.创建ReadExcelL类，导入读取表格代码并进行修改 2.导入jar包 3.创建Usre类，创建表格属性

--- src/User.java
+++ src/User.java
@@ -1,2 +1,41 @@
 public class User {
+    private String username;
+    private String userpassword;
+    private String address;
+    private String phone;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpassword() {
+        return userpassword;
+    }
+
+    public void setUserpassword(String userpassword) {
+        this.userpassword = userpassword;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPassword(String value) {
+    }
 }
