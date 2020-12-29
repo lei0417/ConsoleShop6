5abc09c0ac24e75f0743ae6235ad0a49d1c0ab58
lei0417 <2552625354@qq.com>
2020-12-29 16:33:21 +0800

    修改读取Excel的方式

--- src/Test.java
+++ src/Test.java
@@ -1,27 +1,24 @@
 import java.io.File;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws ClassNotFoundException {
         System.out.println("请输入用户名：");
         Scanner sc = new Scanner(System.in);
         String username = sc.next();
-        System.out.println("您输入的用户名是：" + username);
-
         System.out.println("请输入密码：");
         String password = sc.next();
-        System.out.println("您输入的密码是：" + password);
 
-        File file = new File("C:\\Users\\雷斌\\IdeaProjects\\ConsoleShop\\src\\users.xlsx");
+        //File file = new File("C:\\Users\\雷斌\\IdeaProjects\\ConsoleShop\\src\\users.xlsx");
+        InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
         ReadExcel readExcel = new ReadExcel(); //创建对象
-        readExcel.readExcel(file);
-        User users[] = readExcel.readExcel(file);
-        for (int i = 0;i<users.length;i++){
-            if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())){
+        User users[] = readExcel.readExcel(in);
+        for (int i = 0; i < users.length; i++) {
+            if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
                 System.out.println("登入成功");
                 break;
-            }else {
+            } else {
                 System.out.println("登入失败");
             }
         }
